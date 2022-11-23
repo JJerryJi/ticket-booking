3b0a0767b089a0c6c2df5b43eab9f9753e863ee1
JJerryJi <jerryji040506@ucla.edu>
2022-11-22 18:21:26 -0800

    Improve efficiency

--- main.py
+++ main.py
@@ -15,19 +15,21 @@ driver = webdriver.Chrome()
 
 driver.get("https://secure.recreation.ucla.edu/booking#")
 
+WebDriverWait(driver, 100).until(lambda x: x.find_element(By.ID, "loginLink")).click()
 
-driver.find_element(By.ID,"loginLink").click()
-
-time.sleep(3)
+#need to improve efficiency
+time.sleep(1)
 
 driver.find_element(By.XPATH,"//*[@id='section-sign-in-first']/div[6]/div/button/span/span[2]").click()
 
-time.sleep(3)
-
 driver.find_element(By.ID,"logon").send_keys(username)
-driver.find_element(By.ID,"pass").send_keys(password + Keys.ENTER)
 
-WebDriverWait(driver, 20).until(lambda x: x.find_element(By.ID, 'container-image-item booking-product-image')).click()
+driver.implicitly_wait(10)
+
+WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID,"pass").send_keys(password + Keys.ENTER))
+
+driver.implicitly_wait(10)
+WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, 'container-image-item booking-product-image')).click()
 
 time.sleep(2)
 driver.close()
